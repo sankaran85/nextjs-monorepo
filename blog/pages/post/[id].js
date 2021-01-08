9cffcf5df6aeb6066cfa14ee747f932f640371d0
sankaran <sankaran@totient.co.in>
2021-01-08 23:43:24 +0530

    test commit blog

--- blog/pages/post/[id].js
+++ blog/pages/post/[id].js
@@ -1,12 +1,22 @@
+import React, { useState } from 'react';
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 export default function Post() {
   const router = useRouter()
+  const [count, setCount] = useState('');
+
+  function showtext(e) {
+    console.log(e.target.value);
+    e.target.value;
+    setCount(e.target.value);
+  }
+
   return (
     <div>
       <h3>Post #{router.query.id}</h3>
-      <p>Lorem ipsum</p>
+      <p>Lorem ipsum one poda {count}</p>
+      <input type="text" onChange={showtext} />
       <Link href="/">
         <a>Back to blog dude</a>
       </Link>
