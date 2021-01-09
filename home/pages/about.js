cef9b2102af6098943a778cf78252600cf94d332
sankaran <sankaran@totient.co.in>
2021-01-09 10:52:38 +0530

    changed home page content

--- home/pages/about.js
+++ home/pages/about.js
@@ -1,8 +1,13 @@
 import Link from 'next/link'
-
+import Head from "next/head";
+import styles from '../styles/about.module.scss';
 const About = () => (
   <div>
-    <p>This is the about page.</p>
+    <Head>
+        <title>About</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    <p className={styles.container}>This is the about page new da.</p>
     <div>
       <Link href="/">
         <a>Go Back</a>
